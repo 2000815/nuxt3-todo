b77b209f607b98d2dbf1b71eaefec2a53671be07
2000815 <keisi815keisi@gmail.com>
2023-12-29 13:55:39 +0900

    todolist作成

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="task">
+    <input type="text" v-model="task.title" placeholder="タスクを入力" />
+    <button @click="addTask">追加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: {
+        title: "",
+      },
+    };
+  },
+  methods: {
+    addTask() {
+      const tasks = this.$store.state.tasks;
+
+      tasks.push(this.task);
+
+      this.$store.commit("updateTasks", tasks);
+    },
+  },
+};
+</script>
