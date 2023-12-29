db5d39b96c6a1db2283903a0652d56299591f2df
2000815 <keisi815keisi@gmail.com>
2023-12-29 14:58:37 +0900

    コンポーネントの分割

--- app.vue
+++ app.vue
@@ -1,50 +1,18 @@
 <template>
-  <input v-model="state.newTodoLabel" />
-  <button @click="addTodo">add</button>
-
-  <input id="all" type="radio" v-model="filter" value="all" />
-  <label for="all">すべて</label>
-  <input id="finished" type="radio" v-model="filter" value="finished" />
-  <label for="finished">完了済み</label>
-  <input id="unfinished" type="radio" v-model="filter" value="unfinished" />
-  <label for="unfinished">未完了</label>
-
-  <ul>
-    <li
-      :class="{ todo: true, finished: todo.finished }"
-      :key="todo.label"
-      v-for="todo in filteredTodos"
-      v-text="todo.label"
-      @click="toggleTodo(todo.id)"
-    />
-  </ul>
+  <div>
+    <AddTodo />
+    <TodoList />
+  </div>
 </template>
 
-<script setup lang="ts">
-import { storeToRefs } from "pinia";
-import { reactive } from "vue";
-import { useTodoStore } from "@/src/store/todoListStore";
+<script>
+import AddTodo from "@/src/components/AddTodo.vue";
+import TodoList from "@/src/components/TodoList.vue";
 
-const state = reactive({ newTodoLabel: "" });
-const store = useTodoStore();
-const { filteredTodos, filter } = storeToRefs(store);
-
-const toggleTodo = (id: number) => store.toggleTodo(id);
-const addTodo = () => {
-  if (state.newTodoLabel !== "") {
-    store.addTodo(state.newTodoLabel);
-    state.newTodoLabel = "";
-  }
+export default {
+  components: {
+    AddTodo,
+    TodoList,
+  },
 };
 </script>
-
-<style scoped>
-.todo {
-  user-select: none;
-  cursor: pointer;
-}
-.todo.finished {
-  text-decoration: line-through;
-  color: gray;
-}
-</style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="newTodoLabel" />
+    <button @click="addTodo">Add</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useTodoStore } from "@/src/store/todoListStore";
+
+const newTodoLabel = ref("");
+const store = useTodoStore();
+
+const addTodo = () => {
+  if (newTodoLabel.value !== "") {
+    store.addTodo(newTodoLabel.value);
+    newTodoLabel.value = "";
+  }
+};
+</script>
