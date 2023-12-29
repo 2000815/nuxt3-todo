086f758611327ea1df907cbb0c7c662a954cb27a
2000815 <keisi815keisi@gmail.com>
2023-12-29 13:55:17 +0900

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task">
+    <input type="text" v-model="task.title" placeholder="タスクを入力" />
+    <button @click="deleteTask">削除</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: {
+        title: "",
+      },
+    };
+  },
+  methods: {
+    deleteTask() {
+      this.$store.commit("deleteTask", this.task);
+    },
+  },
+};
+</script>
